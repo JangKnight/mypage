61aed3254bffae473793f4a47f3b9bc41cf4df93
Professor Knight <profdevent@gmail.com>
2026-02-24 21:29:28 -0500

    add templates to default landing page

--- mypage/urls.py
+++ mypage/urls.py
@@ -20,6 +20,6 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.me),
+    path('<str:page>/', views.me),
     path("journal/", include("journal.urls"))
 ]
--- mypage/views.py
+++ mypage/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def me(request):
-    return HttpResponse("<h1>About Me</h1>")
+def me(request, page):
+    if page == None or page == "":
+        page = "home"
+    if page == "home":
+        title = "Home"
+        content = "Welcome to my homepage!"
+    elif page == "about":
+        title = "About"
+        content = "This is the about page."
+    elif page == "contact":
+        title = "Contact"
+        content = "You can contact me at anthonysjhenry@icloud.com."
+    else:
+        title = "Page Not Found"
+        content = "Sorry, the page you are looking for does not exist."
+    return render(request, "main.html", {"title": title, "content": content})
 
